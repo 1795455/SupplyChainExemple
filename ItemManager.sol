673bd5d14fcd1afb21f00cc0a4f3f781ff634428
Hugo Hernan Ruiz <1795455@bdeb.qc.ca>
2020-12-01 22:43:28 -0500

    Step 01

--- ItemManager.sol
+++ ItemManager.sol
@@ -4,9 +4,9 @@ contract ItemManager{
  enum SupplyChainSteps{Created, Paid, Delivered}
 
  struct S_Item {
- ItemManager.SupplyChainSteps _step;
- string _identifier;
- uint _priceInWei;
+    ItemManager.SupplyChainSteps _step;
+    string _identifier;
+    uint _priceInWei;
  }
  mapping(uint => S_Item) public items;
  uint index;
@@ -15,26 +15,23 @@ contract ItemManager{
 
  function createItem(string memory _identifier, uint _priceInWei) public {
 
- items[index]._priceInWei = _priceInWei;
- items[index]._step = SupplyChainSteps.Created;
- items[index]._identifier = _identifier;
- emit SupplyChainStep(index, uint(items[index]._step));
- index++;
+    items[index]._priceInWei = _priceInWei;
+    items[index]._step = SupplyChainSteps.Created;
+    items[index]._identifier = _identifier;
+    emit SupplyChainStep(index, uint(items[index]._step));
+    index++;
  }
 
  function triggerPayment(uint _index) public payable {
- require(items[index]._priceInWei <= msg.value, "Not fully paid");
- require(items[index]._step == SupplyChainSteps.Created, "Item is further in the su
-pply chain");
- items[_index]._step = SupplyChainSteps.Paid;
- emit SupplyChainStep(_index, uint(items[_index]._step));
+    require(items[index]._priceInWei <= msg.value, "Not fully paid");
+    require(items[index]._step == SupplyChainSteps.Created, "Item is further in the supply chain");
+    items[_index]._step = SupplyChainSteps.Paid;
+    emit SupplyChainStep(_index, uint(items[_index]._step));
  }
 
  function triggerDelivery(uint _index) public {
- require(items[_index]._step == SupplyChainSteps.Paid, "Item is further in the supp
-ly chain");
- items[_index]._step = SupplyChainSteps.Delivered;
- emit SupplyChainStep(_index, uint(items[_index]._step));
+    require(items[_index]._step == SupplyChainSteps.Paid, "Item is further in the supply chain");
+    items[_index]._step = SupplyChainSteps.Delivered;
+    emit SupplyChainStep(_index, uint(items[_index]._step));
  }
 }
-With
